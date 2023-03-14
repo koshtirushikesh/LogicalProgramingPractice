a859a04105e356781f9e5ff81dbb3ee1ddd4ecb4
koshtirushikesh <koshtirushikesh3@gmail.com>
2023-03-14 10:23:19 +0530

    [Rushi]: Even or Odd

---
+++ ConsoleApp23/Class2.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace ConsoleApp23
+{
+    internal class Class2
+    {
+        public void EvenOdd()
+        {
+            int num = 0;
+            for (int i = 0; i < num; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    //Console.WriteLine("Even"+i);
+                }
+                else
+                    Console.WriteLine("odd" + i);
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp23/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class2 class2 = new Class2();
+            class2.EvenOdd();
+            Console.ReadLine();
+        }
+    }
+}
